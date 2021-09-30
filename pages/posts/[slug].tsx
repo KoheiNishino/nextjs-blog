6c5a66a9eec46925aab599b5b9c330112122a086
KoheiNishino <koheinishino.95@gmail.com>
2021-10-01 08:30:22 +0900

    remove og:image from post's meta info

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -31,10 +31,7 @@ const Post = ({ post }: Props) => {
           <>
             <article className="mb-32">
               <Head>
-                <title>
-                  {post.title} | {BLOG_TITLE}
-                </title>
-                <meta property="og:image" content={post.ogImage.url} />
+                <title>{post.title} | {BLOG_TITLE}</title>
               </Head>
               <PostHeader
                 title={post.title}
