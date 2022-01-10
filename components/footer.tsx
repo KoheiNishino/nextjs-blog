d2af2976f8e5ed0c507b53cc74c2fa325148ae1a
KoheiNishino <koheinishino.95@gmail.com>
2022-01-10 11:18:09 +0900

    change the footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,11 +1,9 @@
-import { BLOG_TITLE } from '../lib/constants'
-
 const Footer = () => {
   return (
     <footer className="border-t border-accent-2 py-7 text-gray-400">
       <div className="flex flex-col items-center">
         <div className="text-sm md:text-lg">
-          © 2021, {BLOG_TITLE} All rights reserved.
+          © {new Date().getFullYear()} Kohei Nishino
         </div>
       </div>
     </footer>
