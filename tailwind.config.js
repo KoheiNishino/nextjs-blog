a2dec3626db6063a7bdaeb241ec0fc7c1a0bab93
KoheiNishino <koheinishino.95@gmail.com>
2022-01-30 19:59:05 +0900

    change extend colors in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,9 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'accent-1': '#FAFAFA',
-        'accent-2': '#EAEAEA',
-        'accent-7': '#333',
-        success: '#0070f3',
-        cyan: '#79FFE1',
+        'link': '#1d9bf0',
+        'primary': '#ffffff',
+        'secondary': '#B3B3B3',
       },
       spacing: {
         28: '7rem',
