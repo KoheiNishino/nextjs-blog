267fa1fe0cb545d01aee59036fb446afeb97126a
KoheiNishino <koheinishino.95@gmail.com>
2022-02-20 13:17:49 +0900

    add remark breaks

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,7 +1,8 @@
 import remark from 'remark'
+import breaks from 'remark-breaks'
 import html from 'remark-html'
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown)
+  const result = await remark().use(breaks).use(html).process(markdown)
   return result.toString()
 }
