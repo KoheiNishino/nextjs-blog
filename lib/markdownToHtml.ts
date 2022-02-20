bb45240cc96b02c10226ced45ea4bd52355567ff
KoheiNishino <koheinishino.95@gmail.com>
2022-02-20 13:18:49 +0900

    add remark highlight

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,8 +1,9 @@
 import remark from 'remark'
 import breaks from 'remark-breaks'
+import hljs from 'remark-highlight.js'
 import html from 'remark-html'
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(breaks).use(html).process(markdown)
+  const result = await remark().use(breaks).use(hljs).use(html).process(markdown)
   return result.toString()
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import GoogleAnalytics from '../components/google-analytics'
 import Layout from '../components/layout'
 import usePageView from '../hooks/usePageView'
 import '../styles/index.css'
+import 'highlight.js/styles/stackoverflow-dark.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   usePageView()
