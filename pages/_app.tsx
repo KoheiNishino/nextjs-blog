3dea0b89d5011603797396978748b3d09142b807
KoheiNishino <koheinishino.95@gmail.com>
2022-02-23 09:44:06 +0900

    Revert "add remark highlight"
    This reverts commit bb45240cc96b02c10226ced45ea4bd52355567ff.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,6 @@ import GoogleAnalytics from '../components/google-analytics'
 import Layout from '../components/layout'
 import usePageView from '../hooks/usePageView'
 import '../styles/index.css'
-import 'highlight.js/styles/stackoverflow-dark.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   usePageView()
