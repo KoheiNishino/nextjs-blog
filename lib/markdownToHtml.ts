e246c18b665d8e3cc84c06aa435e4233b9ee7e37
KoheiNishino <koheinishino.95@gmail.com>
2022-02-23 09:44:20 +0900

    Revert "add remark breaks"
    This reverts commit 267fa1fe0cb545d01aee59036fb446afeb97126a.

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,8 +1,7 @@
 import remark from 'remark'
-import breaks from 'remark-breaks'
 import html from 'remark-html'
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(breaks).use(html).process(markdown)
+  const result = await remark().use(html).process(markdown)
   return result.toString()
 }
